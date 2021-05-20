f0ff801ad1c79236999922975597d01c20f39568
avdim <avdimdv@gmail.com>
2021-05-20 14:15:56 +0200

    commit message

---
+++ plugin.video.shura.tv/pagedownfix.py
@@ -0,0 +1,11 @@
+﻿# -*- coding: utf-8 -*
+#/*
+import string, xbmc, xbmcgui, xbmcplugin, os, xbmcaddon
+
+if 'tvshka' in xbmc.Player().getPlayingFile():
+	xbmc.Player().playprevious()
+	xbmc.log('[PageDownFixExceputed] in SHURA.TV')
+else:
+	xbmc.executebuiltin('PlayerControl(Previous)')
+	xbmc.log('[PageDownFixExceputed] not in SHURA.TV')
+#xbmc.log('[PageDownFixExceputed]')
